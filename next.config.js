af827360c579a31c51dadcbfe0a41d67741f3add
ike <ikeno.7g6js@simplelogin.com>
2023-12-08 11:44:42 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true
+  swcMinify: true,
+  env: {
+    LIVEKIT_API_KEY:"APIeXJJcaDKk5f3",
+    LIVEKIT_API_SECRET:"JuMxv4f4gkVZNerbCVLuv3Fp0wVTUelA8fgCeQH9XinE",
+    LIVEKIT_WS_URL:"wss://chatroom-a73rh25f.livekit.cloud"
+  }
 };
 
 module.exports = nextConfig;
